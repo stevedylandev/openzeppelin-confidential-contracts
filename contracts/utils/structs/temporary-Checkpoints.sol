f281da82329e045d146646f4d48a474b5e6ad2bf
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2025-07-26 00:05:21 +0200

    Update `@openzeppelin/contracts` and `@[...]/contracts-upgradeable` to `v5.4.0` (#111)
    * Update `@openzeppelin/contracts` and `@[...]/contracts-upgradeable` to `v5.4.0`
    
    * add tag in .gitmodules
    
    ---------
    
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- contracts/utils/structs/temporary-Checkpoints.sol
+++ contracts/utils/structs/temporary-Checkpoints.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (utils/structs/Checkpoints.sol)
+// OpenZeppelin Contracts (last updated v5.4.0) (utils/structs/Checkpoints.sol)
 // This file was procedurally generated from scripts/generate/templates/Checkpoints.js.
 // WARNING: This file is temporary and will be deleted once the latest version of the file is released in v5.5.0 of @openzeppelin/contracts.
 
