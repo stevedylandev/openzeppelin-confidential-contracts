236b78b5992230d1f63ed0670c3557fb1ae31b08
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-14 11:25:34 -0400

    validate pragmas (#129)
    * validate pragmas
    
    * lint fix
    
    * just check
    
    * add back files
    
    * fix pragma
    
    * Update scripts/checks/pragma-validity.js
    
    Co-authored-by: James Toussaint <33313130+james-toussaint@users.noreply.github.com>
    
    * add symlink comments
    
    ---------
    
    Co-authored-by: James Toussaint <33313130+james-toussaint@users.noreply.github.com>

---
+++ scripts/solc-versions.js
@@ -0,0 +1,16 @@
+// TODO: Replace file content with symlink to lib/openzeppelin-contracts when available.
+const { exec } = require('child_process');
+const semver = require('semver');
+const { range } = require('./helpers');
+
+module.exports = {
+  versions: ['0.4.26', '0.5.16', '0.6.12', '0.7.6', '0.8.30']
+    .map(semver.parse)
+    .flatMap(({ major, minor, patch }) => range(patch + 1).map(p => `${major}.${minor}.${p}`)),
+  compile: (source, version) =>
+    new Promise((resolve, reject) =>
+      exec(`forge build ${source} --use ${version} --out out/solc-${version}`, error =>
+        error ? reject(error) : resolve(),
+      ),
+    ),
+};
